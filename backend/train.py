479e625d992ff72f02312284a6a7f0b3362f608d
Joshua Huliganga <jhuliganga2000@gmail.com>
2024-10-27 08:03:59 -0400

    update

--- backend/train.py
+++ backend/train.py
@@ -9,25 +9,25 @@ from LoadData import load_data_URL, prepare_data_URL, process_data_EMAIL
 # *************** FOR URL ***************
 
 
-# Load and preprocess URL data
-data_URL = load_data_URL()
-X_URL, y_URL, vectorizer_URL = prepare_data_URL(data_URL)
+# # Load and preprocess URL data
+# data_URL = load_data_URL()
+# X_URL, y_URL, vectorizer_URL = prepare_data_URL(data_URL)
 
-# Split the dataset
-X_train_URL, X_test_URL, y_train_URL, y_test_URL = train_test_split(X_URL, y_URL, test_size=0.2, random_state=42)
+# # Split the dataset
+# X_train_URL, X_test_URL, y_train_URL, y_test_URL = train_test_split(X_URL, y_URL, test_size=0.2, random_state=42)
 
-# Initialize and train the model
-model_URL = RandomForestClassifier(n_estimators=100, random_state=42)
-model_URL.fit(X_train_URL, y_train_URL)
+# # Initialize and train the model
+# model_URL = RandomForestClassifier(n_estimators=100, random_state=42)
+# model_URL.fit(X_train_URL, y_train_URL)
 
-# Make predictions and evaluate the model
-predictions_URL = model_URL.predict(X_test_URL)
-print("Accuracy:", accuracy_score(y_test_URL, predictions_URL))
-print("Classification Report:\n", classification_report(y_test_URL, predictions_URL))
+# # Make predictions and evaluate the model
+# predictions_URL = model_URL.predict(X_test_URL)
+# print("Accuracy:", accuracy_score(y_test_URL, predictions_URL))
+# print("Classification Report:\n", classification_report(y_test_URL, predictions_URL))
 
-# Save the model and vectorizer
-joblib.dump(model_URL, 'models/url_detector_model.pkl')
-joblib.dump(vectorizer_URL, 'models/vectorizer.pkl')
+# # Save the model and vectorizer
+# joblib.dump(model_URL, 'models/url_detector_model.pkl')
+# joblib.dump(vectorizer_URL, 'models/vectorizer.pkl')
 
 
 # *************** FOR EMAIL ***************
@@ -48,5 +48,5 @@ print("Accuracy:", accuracy_score(y_test_EMAIL, predictions_EMAIL))
 print("Classification Report:\n", classification_report(y_test_EMAIL, predictions_EMAIL))
 
 # Save the model and vectorizer
-joblib.dump(model_URL, 'models/email_detector_model.pkl')
-joblib.dump(vectorizer_URL, 'models/vectorizer_email.pkl')
+joblib.dump(model_EMAIL, 'models/email_detector_model.pkl')
+joblib.dump(vectorizer_EMAIL, 'models/vectorizer_email.pkl')
